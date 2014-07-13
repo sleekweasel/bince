852f67802471f2f2b38ba71acb1737f223e9be1b
SleekWeasel <sleekweasel@gmail.com>
2014-07-13 23:45:13 +0100

    Works

---
+++ wear/src/main/java/uk/org/baverstock/bince/BinceBox.java
@@ -0,0 +1,27 @@
+package uk.org.baverstock.bince;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+public class BinceBox extends Activity {
+
+    private static final String TAG = BinceBox.class.getSimpleName();
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bince_box);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+                Log.d(TAG, "TextView: " + mTextView.getText() + " view=" + mTextView);
+            }
+        });
+    }
+}
